b878fc07311bac895951f800a92178ee265ae517
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-09-01 11:56:21 -0700

    [UPDATE] Tests are now passing

--- src/index.js
+++ src/index.js
@@ -8,93 +8,93 @@ function capitalize(str) {
 // Challenge 2
 // allCaps(): All characters uppercase.
 function allCaps(str) {
-    return str.toUpperCase();
+  return str.toUpperCase();
 }
   
 // Challenge 3
 //capitalizeWords(): First character of each word uppercase.
 function capitalizeWords(str) {
-    return str
-      .split(" ")
-      .map((word) => capitalize(word))
-      .join(" ");
+  return str
+    .split(" ")
+    .map((word) => capitalize(word))
+    .join(" ");
 }
   
 // Challenge 4
 // removeExtraSpaces(): Removes all spaces from the beginning and end of a String along with any extra spaces in the middle.
 function removeExtraSpaces(str) {
-    return str.trim().split(/\s+/).join(" ");
+  return str.trim().split(/\s+/).join(" ");
 }
 
 // Challenge 5: kebobCase(): Removes extra spaces and replaces spaces with the hyphen "-", and makes all characters lowercase.
-function kebobCase(str) {
-    str = str
-      .toLowerCase()
-      .split("")
-      .filter((char) => {
-        const code = char.charCodeAt(0);
-        return (
-          (code >= 97 && code <= 122) ||
-          (code >= 48 && code <= 57) ||
-          code === 32 ||
-          code === 45
-        );
-      })
-      .join("");
-  
-    return removeExtraSpaces(str).split(" ").join("-");
+function kebobCase(str, separator = '-') {
+  const lower = str.toLowerCase();
+  const split = lower.split('');
+  const filtered = split.filter((c) => {
+    const code = c.charCodeAt(0);
+    if (code > 96 && code < 123) {
+      return true;
+    } if (code > 47 && code < 58) {
+      return true;
+    } if (code === 32 || code === separator.charCodeAt(0)) {
+      return true;
+    }
+    return false;
+  });
+
+  const removeSpace = removeExtraSpaces(filtered.join(''));
+  return removeSpace.split(' ').join(separator);
 }
   
 // Challenge 6
 // snakeCase(): Removes extra space and replaces spaces with an underscore "_", and makes all characters lowercase.
 function snakeCase(str) {
-    return kebobCase(str).replace(/-/g, "_");
+  return kebobCase(str, '_');
 }
   
 // Challenge 7
 // camelCase(): Lowercases the first character of the first word. Then uppercases the first character of all other words, and removes all spaces.
 function camelCase(str) {
-    const words = str.split(" ");
-    return (
-      words[0].toLowerCase() +
-      words
-        .slice(1)
-        .map((word) => capitalize(word))
-        .join("")
-    );
+  const words = str.split(" ");
+  return (
+    words[0].toLowerCase() + words
+      .slice(1)
+      .map((word) => capitalize(word))
+      .join("")
+  );
 }
   
 // Challenge 8
 // shift() - Takes the first character of a string and moves to the end of a string.
 function shift(str, n = 1) {
-    return str.slice(n) + str.slice(0, n);
+  return str.slice(n) + str.slice(0, n);
 }
   
 // Challenge 9
 // makeHashTag(): Capitalizes all words and adds a hashtag to the beginning.
 function makeHashTag(str) {
-    const words = str
-      .split(" ")
-      .sort((a, b) => b.length - a.length)
-      .slice(0, 3);
-    return words.map((word) => "#" + word.toLowerCase());
+  const words = str.split(' ');
+  const sortWords = words.sort((a, b) => b.length - a.length);
+  const notemtyWords = sortWords.filter((word) => word !== '');
+  const hashTags = notemtyWords.slice(0, 3).map((word) => `#${word.toLowerCase()}`);
+  return hashTags;
 }
   
 // Challenge 10:
 // isEmpty(): Returns true if the given string is empty or contains only whitespace. White space includes: spaces, line returns, and tabs.
 function isEmpty(str) {
-    return str.trim() === "";
+  return str.trim() === "";
 }
 
 module.exports = {
-    capitalize,
-    allCaps,
-    capitalizeWords,
-    removeExtraSpaces,
-    kebobCase,
-    snakeCase,
-    camelCase,
-    shift,
-    makeHashTag,
-    isEmpty,
-  };
+  capitalize,
+  allCaps,
+  capitalizeWords,
+  removeExtraSpaces,
+  kebobCase,
+  snakeCase,
+  camelCase,
+  shift,
+  makeHashTag,
+  isEmpty,
+};
--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,6 @@
 const stringTest = require('../src/index.js');
 
+// Challenge 1
 test('capitalize', () => {
     const firstOutput = stringTest.capitalize('hello');
     expect(firstOutput).toBe('Hello');
@@ -11,6 +12,7 @@ test('capitalize', () => {
     expect(thirdOutput).toBe('');
 });
 
+// Challenge 2
 test('allCaps', () => {
     const firstOutput = stringTest.allCaps('hello');
     expect(firstOutput).toBe('HELLO');
@@ -22,6 +24,7 @@ test('allCaps', () => {
     expect(thirdOutput).toBe('');
 });
 
+// Challenge 3
 test('capitalizeWords', () => {
     const firstOutput = stringTest.capitalizeWords('do all the things');
     expect(firstOutput).toBe('Do All The Things');
@@ -30,6 +33,7 @@ test('capitalizeWords', () => {
     expect(secondOutput).toBe('');
 });
 
+// Challenge 4
 test('removeExtraSpaces', () => {
     const firstOutput = stringTest.removeExtraSpaces('   Hello    world!   ');
     expect(firstOutput).toBe('Hello world!');
@@ -38,22 +42,25 @@ test('removeExtraSpaces', () => {
     expect(secondOutput).toBe('');
 });
 
-test('kebob', () => {
-    const firstOutput = stringTest.kebob('   Hello    world   ');
+// Challenge 5
+test('kebobCase', () => {
+    const firstOutput = stringTest.kebobCase('   Hello    world   ');
     expect(firstOutput).toBe('hello-world');
 
-    const secondOutput = stringTest.kebob('');
+    const secondOutput = stringTest.kebobCase('');
     expect(secondOutput).toBe('');
 });
 
-test('snake', () => {
-    const firstOutput = stringTest.snake('  what the    heck   ');
-    expect(firstOutput).toBe('what_the_heck');
+// Challenge 6
+test('snakeCase', () => {
+    const firstOutput = stringTest.snakeCase('  this  is   snake');
+    expect(firstOutput).toBe('this_is_snake');
 
-    const secondOutput = stringTest.snake('');
+    const secondOutput = stringTest.snakeCase('');
     expect(secondOutput).toBe('');
 });
 
+// Challenge 7
 test('camelCase', () => {
     const firstOutput = stringTest.camelCase('Camel Case');
     expect(firstOutput).toBe('camelCase');
@@ -62,6 +69,7 @@ test('camelCase', () => {
     expect(secondOutput).toBe('');
 });
 
+// Challenge 8
 test('shift function', () => {
     const firstOutput = stringTest.shift('Hello World');
     expect(firstOutput).toBe('ello WorldH');
@@ -70,6 +78,7 @@ test('shift function', () => {
     expect(secondOutput).toBe('');
 });
 
+// Challenge 9
 test('makeHashTag', () => {
     const firstOutput = stringTest.makeHashTag('Amazing bongo drums for sale');
     expect(firstOutput).toEqual(['#amazing', '#bongo', '#drums']);
@@ -78,6 +87,7 @@ test('makeHashTag', () => {
     expect(secondOutput).toEqual([]);
 });
 
+// Challenge 10
 test('isEmpty', () => {
     const firstOutput = stringTest.isEmpty('Abc def');
     expect(firstOutput).toBe(false);
